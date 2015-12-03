0b6b22c14d34fb284f845992dcfd267fcf1ed5a5
Andrés Lou <lourios.andres@gmail.com>
2015-12-03 01:27:58 -0500

    additional documentation

--- subsets.py
+++ subsets.py
@@ -1,10 +1,24 @@
+#################################################
+##Semantic quantifier calculator                #
+##by Andrés Lou, 2015                          #
+##Creative Commons Attribution License (cc-by)  #
+#################################################
+
+
 from itertools import chain, combinations
 
+##powerset takes a string converts it into a iterator
+##that defines a powerset utilising each character as
+##an element. Only used internally.
 def powerset(iterable):
     xs = list(iterable)
     # note we return an iterator rather than a list
     return chain.from_iterable(combinations(xs,n) for n in range(len(xs)+1))
 
+##convertToPowerSets takes two strings, A and B, and
+##returns A as a powerset set object and B as a frozenset
+##object. A is meant to represent the universe of discourse
+##and B the quantified noun.
 def convertToPowerSets(Astr,Bstr):
     Aprime = list(powerset(Astr))
     B = set(Bstr)
@@ -17,6 +31,9 @@ def convertToPowerSets(Astr,Bstr):
 
     return A,B
 
+##All Q methods take to parameters, a powerset A
+##of the universe of discourse, A, and the set of
+##the interpretation of the quantified noun, B
 def Qmost(A,B):
     listOfSets = []
     for subset in A:
@@ -54,3 +71,47 @@ def Qno(A,B):
         if subset.intersection(B) == set():
             listOfSets.append(list(subset))
     return listOfSets
+
+###EXAMPLE OF USE###
+
+#M = <{l, m, n, o, p}, i> , where i has the following graph:
+#John ----->    n
+#Coco ----->    l
+#monkeys -->    {l,m,p}
+#teachers ->    {n,o}
+#laughed -->    {n,o}
+
+#>>> A,B = convertToPowerSets("lmnop", "lmp")
+
+#“Most monkeys”
+
+#>>> mostAB = Qmost(A,B)
+
+#“No monkeys”
+
+#>>> noAB = Qno(A,B)
+
+##>>> for i in mostAB: i
+##
+##['p', 'm']
+##['p', 'l', 'm']
+##['p', 'n', 'l', 'm']
+##['o', 'p', 'l']
+##['n', 'l', 'm']
+##['m']
+##['l', 'm']
+##['o', 'p', 'l', 'm']
+##['o', 'n', 'l', 'm', 'p']
+##['o', 'l', 'm']
+##['p', 'n', 'l']
+##['p', 'l']
+##['o', 'p', 'm']
+##['p', 'n', 'm']
+##['l']
+##['p']
+
+##>>> for i in noAB: i
+##
+##['o', 'n']
+##['o']
+##['n']
